84eb0783578163c1b10a23e0ff6d1da0e92a7b00
firasbelhiba <firas.belhiba@esprit.tn>
2022-12-14 01:02:20 +0100

    Adding logging

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,11 +1,14 @@
-import { Context, storage } from "near-sdk-as";
+import { Context, storage, logging } from "near-sdk-as";
 
 export class Greeting {
   getGreeting(accountId: string): string | null {
+    logging.log("Greeting for " + accountId);
+
     return storage.get<string>(accountId, "Greeting not available");
   }
 
   setGreeting(greeting: string): void {
+    logging.log("Setting Greeting for " + Context.sender);
     storage.set(Context.sender, greeting);
   }
 }
